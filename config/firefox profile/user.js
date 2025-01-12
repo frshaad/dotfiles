1722cef098febd4d58333726797ba05f85d16241
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-01-12 12:03:32 +0330

    update Firefox user preferences for enhanced privacy and disable unnecessary features; add file nesting patterns in VSCode settings

--- config/firefox profile/user.js
+++ config/firefox profile/user.js
@@ -1,89 +1,43 @@
-user_pref("network.cookie.sameSite.noneRequiresSecure", true);
-user_pref("dom.security.https_only_mode", true);
-
-/** SEARCH / URL BAR ***/
-user_pref("browser.urlbar.untrimOnUserInteraction.featureGate", true);
-user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
-user_pref("browser.urlbar.update2.engineAliasRefresh", true);
-user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
-user_pref("browser.formfill.enable", false);
-// PREF: disable live search suggestions (Google, Bing, etc.)
-user_pref("browser.search.suggest.enabled", false);
-// PREF: disable Firefox Suggest
-user_pref("browser.urlbar.quicksuggest.enabled", false); // controls whether the UI is shown
-user_pref("browser.urlbar.groupLabels.enabled", false);
-
-/** MIXED CONTENT + CROSS-SITE ***/
-user_pref("pdfjs.enableScripting", false);
-
-/** CRASH REPORTS ***/
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
-
-/** MOZILLA UI ***/
-// PREF: Mozilla VPN
-user_pref("browser.privatebrowsing.vpnpromourl", "");
-// disable about:addons' Recommendations pane (uses Google Analytics)
-user_pref("extensions.getAddons.showPane", false); // HIDDEN
-// PREF: disable recommendations in about:addons' Extensions and Themes panes
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
-// PREF: Personalized Extension Recommendations in about:addons and AMO
-user_pref("browser.discovery.enabled", false);
-// PREF: disable Firefox from asking to set as the default browser
-user_pref("browser.shell.checkDefaultBrowser", false);
-// PREF: disable Extension Recommendations (CFR: "Contextual Feature Recommender")
+user_pref("dom.security.https_only_mode", true); // Enable HTTPS-only Mode
+user_pref("browser.search.separatePrivateDefault.ui.enabled", true); // Enable seperate search engine for Private Windows
+user_pref("browser.urlbar.update2.engineAliasRefresh", true); // Enable option to add custom search engine
+user_pref("browser.search.suggest.enabled", false); // // Disable live search suggestions
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false); // Disable Firefox sponored Suggest
+user_pref("browser.urlbar.suggest.recentsearches", false); // Disable recent searches
+user_pref("signon.rememberSignons", false); // Disable password manager
+user_pref("privacy.history.custom", true); // Set History section to show all options
+user_pref("browser.formfill.enable", false); // Disable form history
+user_pref("pdfjs.enableScripting", false); // Prevent PDFs from loading javascript
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false); // Disable Crash Reports
+user_pref("app.shield.optoutstudies.enabled", false); // Disable Studies
+user_pref("browser.privatebrowsing.vpnpromourl", ""); // Disable Mozilla VPN
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false); // Disable pinned shortcuts on new tab
+user_pref("browser.newtabpage.activity-stream.showWeather", false); // Disable Weather widget on new tab
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Disable Recommended by Pocket on new tab
+user_pref("browser.urlbar.quicksuggest.enabled", false); // Disable Recommended by Pocket on new tab
+user_pref("findbar.highlightAll", true); // Show all matches in Findbar
+user_pref("layout.word_select.eat_space_to_next_word", false); // Do not select the space next to a word when selecting a word
+user_pref("privacy.globalprivacycontrol.enabled", true); // Enable Global Privacy Control (GPC)
+user_pref("privacy.userContext.enabled", true); // Enable Container Tabs
+user_pref("privacy.userContext.ui.enabled", true); // Enable Container Tabs UI setting
+user_pref("extensions.pocket.enabled", false); // Disable Pocket
+user_pref("devtools.toolbox.zoomValue", 1.2); // Devtools zoom level
+user_pref("browser.aboutConfig.showWarning", false); // Disable tab and about:config warnings
+user_pref("browser.preferences.moreFromMozilla", false); // Hide "More from Mozilla" in Settings
 user_pref(
   "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons",
   false
-);
+); // Disable extension recommendations
 user_pref(
   "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features",
   false
-);
-// PREF: hide "More from Mozilla" in Settings
-user_pref("browser.preferences.moreFromMozilla", false);
-// PREF: tab and about:config warnings
-user_pref("browser.aboutConfig.showWarning", false);
-// PREF: minimize URL bar suggestions (bookmarks, history, open tabs)
-user_pref("browser.urlbar.suggest.engines", false);
-user_pref("browser.urlbar.suggest.calculator", true);
-user_pref("browser.urlbar.unitConversion.enabled", true);
-// PREF: disable urlbar trending search suggestions [FF118+]
-user_pref("browser.urlbar.trending.featureGate", false);
-// PREF: Pinned Shortcuts on New Tab
-user_pref("browser.newtabpage.activity-stream.feeds.topsites", false); // Shortcuts
-user_pref("browser.newtabpage.activity-stream.showWeather", false); // Weather [FF130+]
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Recommended by Pocket
-
-// PREF: show all matches in Findbar
-user_pref("findbar.highlightAll", true);
-
-// PREF: do not select the space next to a word when selecting a word
-user_pref("layout.word_select.eat_space_to_next_word", false);
-
-// PREF: Global Privacy Control (GPC) [FF118+]
-user_pref("privacy.globalprivacycontrol.enabled", true);
-
-// PREF: set History section to show all options
-user_pref("privacy.history.custom", true);
-// PREF: disable search and form history
-user_pref("browser.formfill.enable", false);
-
-// PREF: enable Container Tabs and its UI setting [FF50+]
-user_pref("privacy.userContext.ui.enabled", true);
-
-/** POCKET ***/
-user_pref("extensions.pocket.enabled", false);
-
-// devtools zoom level (inspector, console)
-user_pref("devtools.toolbox.zoomValue", 1.2); // default: 1
-
-// remove fullscreen delay
-user_pref("full-screen-api.transition-duration.enter", "0 0"); // default=200 200
-user_pref("full-screen-api.transition-duration.leave", "0 0"); // default=200 200
-
-// disable fullscreen notice
-user_pref("full-screen-api.warning.delay", -1); // default=500
-user_pref("full-screen-api.warning.timeout", 0); // default=3000
-
-// disable password manager
-user_pref("signon.rememberSignons", false);
+); // Disable features recommendations
+user_pref("browser.discovery.enabled", false); // Disable personalized extension recommendations in about:addons and AMO
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false); // Disable recommendations in about:addons' Extensions and Themes panes
+user_pref("extensions.getAddons.showPane", false); // Disable about:addons' Recommendations pane
+user_pref("full-screen-api.transition-duration.enter", "0 0"); // Remove fullscreen delay
+user_pref("full-screen-api.transition-duration.leave", "0 0"); // Remove fullscreen delay
+user_pref("full-screen-api.warning.delay", -1); // Disable fullscreen notice
+user_pref("full-screen-api.warning.timeout", 0); // Disable fullscreen notice
+user_pref("browser.urlbar.trending.featureGate", false); // Disable urlbar trending search suggestions
+user_pref("datareporting.healthreport.uploadEnabled", false); // Disable Health Reports
