c1d8e8e10c8bc3c64a5b086ff08be7d6bf385e19
Farshad Hatami <76130454+frshaad@users.noreply.github.com>
2025-01-23 14:16:19 +0330

    update Firefox user preferences to enhance privacy by disabling various features and settings

--- config/firefox profile/user.js
+++ config/firefox profile/user.js
@@ -7,16 +7,12 @@ user_pref("browser.urlbar.suggest.recentsearches", false); // Disable recent sea
 user_pref("signon.rememberSignons", false); // Disable password manager
 user_pref("privacy.history.custom", true); // Set History section to show all options
 user_pref("browser.formfill.enable", false); // Disable form history
-user_pref("pdfjs.enableScripting", false); // Prevent PDFs from loading javascript
-user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false); // Disable Crash Reports
-user_pref("app.shield.optoutstudies.enabled", false); // Disable Studies
 user_pref("browser.privatebrowsing.vpnpromourl", ""); // Disable Mozilla VPN
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false); // Disable pinned shortcuts on new tab
 user_pref("browser.newtabpage.activity-stream.showWeather", false); // Disable Weather widget on new tab
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false); // Disable Recommended by Pocket on new tab
 user_pref("browser.urlbar.quicksuggest.enabled", false); // Disable Recommended by Pocket on new tab
 user_pref("findbar.highlightAll", true); // Show all matches in Findbar
-user_pref("layout.word_select.eat_space_to_next_word", false); // Do not select the space next to a word when selecting a word
 user_pref("privacy.globalprivacycontrol.enabled", true); // Enable Global Privacy Control (GPC)
 user_pref("privacy.userContext.enabled", true); // Enable Container Tabs
 user_pref("privacy.userContext.ui.enabled", true); // Enable Container Tabs UI setting
@@ -35,9 +31,4 @@ user_pref(
 user_pref("browser.discovery.enabled", false); // Disable personalized extension recommendations in about:addons and AMO
 user_pref("extensions.htmlaboutaddons.recommendations.enabled", false); // Disable recommendations in about:addons' Extensions and Themes panes
 user_pref("extensions.getAddons.showPane", false); // Disable about:addons' Recommendations pane
-user_pref("full-screen-api.transition-duration.enter", "0 0"); // Remove fullscreen delay
-user_pref("full-screen-api.transition-duration.leave", "0 0"); // Remove fullscreen delay
-user_pref("full-screen-api.warning.delay", -1); // Disable fullscreen notice
-user_pref("full-screen-api.warning.timeout", 0); // Disable fullscreen notice
 user_pref("browser.urlbar.trending.featureGate", false); // Disable urlbar trending search suggestions
-user_pref("datareporting.healthreport.uploadEnabled", false); // Disable Health Reports
